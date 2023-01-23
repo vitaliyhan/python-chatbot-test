1163e715cd44e5923d0f1b0bdef12be205aeb93b
Vitaiyhan <love-vitaliy@ya.ru>
2023-01-23 06:38:04 +0300

    add

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import config
 import logging
-from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, InlineQueryHandler
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -17,6 +17,22 @@ async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 
+# async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     query = update.inline_query.query
+#     if not query:
+#         return
+#     results = [InlineQueryResultArticle(
+#         id=query.upper(),
+#         title='Caps all text: ' + str(query.upper()),
+#         input_message_content=InputTextMessageContent(query.upper())
+#     )]
+#     await context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
 # красиво логгируем
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -29,9 +45,14 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     caps_handler = CommandHandler('caps', caps)
+    # inline_caps_handler = InlineQueryHandler(inline_caps)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
     # add handler
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
     application.add_handler(caps_handler)
+    # application.add_handler(inline_caps_handler)
+    application.add_handler(unknown_handler)
     # run
     application.run_polling()
